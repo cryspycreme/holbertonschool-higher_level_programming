1abaaca4a3bf8065740818793642fc1ccc47268c
“cryspycreme” <11529@holbertonstudents.com>
2025-09-02 20:31:45 +1000

    Update function for zero values

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
-if number > 0:
+if number >= 0:
     string = "Last digit of {} is {} and is ".format(number, last_digit)
     if last_digit > 5:
         print(string + "greater than 5")
