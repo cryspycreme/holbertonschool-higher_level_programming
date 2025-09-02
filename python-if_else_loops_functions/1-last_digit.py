e9e6231f9bd908dc7ee94bf4bafd0fe01fe41459
“cryspycreme” <11529@holbertonstudents.com>
2025-09-02 20:31:03 +1000

    Update function for negative values

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-string = "Last digit of {} is {} and is ".format(number, last_digit)
+last_digit = abs(number) % 10
 
-if last_digit > 5:
-    print(string + "greater than 5")
-elif last_digit == 0:
-    print(string + "0")
-elif last_digit < 6 and last_digit != 0:
+if number > 0:
+    string = "Last digit of {} is {} and is ".format(number, last_digit)
+    if last_digit > 5:
+        print(string + "greater than 5")
+    elif last_digit == 0:
+        print(string + "0")
+    elif last_digit < 6 and last_digit != 0:
+        print(string + "less than 6 and not 0")
+if number < 0:
+    string = "Last digit of {} is -{} and is ".format(number, last_digit)
     print(string + "less than 6 and not 0")
