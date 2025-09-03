a9875a6cf29ee16e025a4b884ececee91fe6bb4f
“cryspycreme” <11529@holbertonstudents.com>
2025-09-03 15:15:11 +1000

    Add program 9-print_last_digit.oy

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# Print numbers from 0 - 99 (range 0, 100)
+# a. have two loops that prints from 0 - 9
+# if num1 != num 2, concantenate the numbers: print(num1 + num2 ", ", end="")
+# 
