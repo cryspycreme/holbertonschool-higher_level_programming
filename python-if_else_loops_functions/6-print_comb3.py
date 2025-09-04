331ab33e9bd8f1faf1141816cb9a3b4a374f57b3
“cryspycreme” <11529@holbertonstudents.com>
2025-09-04 16:02:11 +1000

    Add program 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-# Print numbers from 0 - 99 (range 0, 100)
-# a. have two loops that prints from 0 - 9
-# if num1 != num 2, concantenate the numbers: print(num1 + num2 ", ", end="")
-# 
+for first_dig in range(10):
+    for sec_dig in range(first_dig + 1, 10):
+        print("{}{}".format(first_dig, sec_dig), end="")
+        if first_dig < 8:
+            print(", ", end="")
+        else:
+            continue
+print("")
