a1c1e030e6a418d36e97211889207b1be437cdbf
“cryspycreme” <11529@holbertonstudents.com>
2025-09-08 17:35:25 +1000

    Add :d format specifier

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for idx in range(len(matrix)):
-        print("{}".format(matrix[idx]))
+        print("{:d}".format(matrix[idx]))
         matrix[idx] = []
