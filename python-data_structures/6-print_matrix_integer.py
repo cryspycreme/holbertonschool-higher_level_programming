fa71962775d56288786a821d1b8ca76e56870132
“cryspycreme” <11529@holbertonstudents.com>
2025-09-08 20:48:57 +1000

    Fix if condition for 1 element

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
-            if (column < len(matrix[row]) - 1):
+            if column < (len(matrix[row]) - 1):
                 print("{:d}".format(matrix[row][column]), end=" ")
             else:
-                print(("{:d}".format(matrix[row][column], end="")))
+                print("{:d}".format(matrix[row][column]))
