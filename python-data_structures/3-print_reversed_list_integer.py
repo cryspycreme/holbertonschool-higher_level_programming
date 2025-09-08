c8a31a52baf63f8f83f549624a89cde45067ccc0
“cryspycreme” <11529@holbertonstudents.com>
2025-09-08 16:14:59 +1000

    Add program 3-print_reversed_list_integer.py

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    reversed_list = list(reversed(my_list))
+    for num in range(len(reversed_list)):
+        print("{:d}".format(reversed_list[num]))
