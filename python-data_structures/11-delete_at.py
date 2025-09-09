da7ba2aaf688b4bfab42bd8a9b95bbf49b220b6c
“cryspycreme” <11529@holbertonstudents.com>
2025-09-09 15:10:10 +1000

    Remove comments from code

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-# If idx is negative or out of range, nothing change (returns the same list)
-# You are not allowed to use pop()
-
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
