cf1890e08e53d3d30c98be9b875a51962920663e
“cryspycreme” <11529@holbertonstudents.com>
2025-09-09 14:10:31 +1000

    Add program 9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#return max integer in the list
+#if list is empty, return None
+
+def max_integer(my_list=[]):
+    sort_list = list(sorted(my_list))
+    if my_list == []:
+        return None
+    else:
+        max_int = sort_list[-1]
+    return max_int
