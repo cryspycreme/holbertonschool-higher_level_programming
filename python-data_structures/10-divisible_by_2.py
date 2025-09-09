7d573272cb031b6d2b2596ecc7a67c5e583036a9
“cryspycreme” <11529@holbertonstudents.com>
2025-09-09 14:55:44 +1000

    Add program 10-divisible_by_2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    new_list = [True if my_list[num] % 2 == 0
+                else False for num in range(len(my_list))]
+
+    return new_list
