fc66fb60d4ec278535f0885a4c85db94abeb45b0
“cryspycreme” <11529@holbertonstudents.com>
2025-09-09 13:57:29 +1000

    Add empty sentence condition

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,9 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    first_ch = sentence[:1]
+    if (length == 0):
+        first_ch = "None"
+    else:
+        first_ch = sentence[:1]
     tuple = (length, first_ch)
     return tuple
