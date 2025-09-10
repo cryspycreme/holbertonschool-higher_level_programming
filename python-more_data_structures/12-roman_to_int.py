3d7fa0a8e140653cfa22d3a7bba64bf762bad64b
“cryspycreme” <11529@holbertonstudents.com>
2025-09-10 20:23:37 +1000

    Add is instance condition

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,8 @@ roman_set = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
+    if not isinstance(roman_string, str):
+        return 0
     roman_list = list(reversed(roman_string))
     total = 0
     prev_value = 0
