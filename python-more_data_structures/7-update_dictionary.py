b55aaf5a1d48784a0185e648a79986741219e7a9
“cryspycreme” <11529@holbertonstudents.com>
2025-09-10 16:35:17 +1000

    Add program 7-udpate_dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# If a key exists in the dictionary, the value will be replaced
+# If a key doesn’t exist in the dictionary, it will be created
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
