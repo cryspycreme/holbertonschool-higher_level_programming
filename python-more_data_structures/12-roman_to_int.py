82c5c33094f1fd165885f51e6cd832609af97644
“cryspycreme” <11529@holbertonstudents.com>
2025-09-10 20:20:06 +1000

    Add reversed string logic and prior value subtract logic

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,19 +6,23 @@ roman_set = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
-    roman_list = list(roman_string)
-    sum = 0
+    roman_list = list(reversed(roman_string))
+    total = 0
+    prev_value = 0
+
     for i in range(len(roman_list)):
-        val = roman_set.get(roman_list[i])
-        if roman_list[i] == 'I' and roman_list[0] == 'I':
-            val = -val
-        sum = val + sum
-    return sum
+        current_val = roman_set.get(roman_list[i])
+        if current_val < prev_value:
+            total = total - current_val
+        else:
+            total = total + current_val
+        prev_value = current_val
+    return total
 
 # define I, V, X, L, C, D in a dict
 # convert the string into an iterable/list so we can iterate over it?
 # loop over the list and use get(key) to return the value of the key
-# if key = I and roman_list[0] == I , then make value = -ve
+# if I is before V, X then subtract -> if current val is < pre_value
 # if I is after V, X then add
 
 # I = 1
