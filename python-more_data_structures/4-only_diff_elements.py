6bffaae7d59c47d96bdf7fbee5f551e66c6587d4
“cryspycreme” <11529@holbertonstudents.com>
2025-09-10 14:47:34 +1000

    Add program 4-only_diff_elements

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Write a function that returns a set of all elements present in only one set.
+
+
+def only_diff_elements(set_1, set_2):
+    set_3 = set_1.union(set_2)
+    new_set = {string for string in set_3 if string in (set_1 ^ set_2)}
+    return new_set
