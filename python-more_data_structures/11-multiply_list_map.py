a7eebb3e52333d2e09b3678b398e607ce35de1b7
“cryspycreme” <11529@holbertonstudents.com>
2025-09-10 19:25:50 +1000

    REduce number of lines

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
-# Initial list should not be modified
-# have to use map
-# Returns a new list:
-# Same length as my_list
-# Each value should be multiplied by number
-
 def multiply_list_map(my_list=[], number=0):
     new_list = list(map(lambda x: x * number, my_list))
     return new_list
