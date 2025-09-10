72cfb32c8d0fc1963a808f0a6e344f4389cd9d59
“cryspycreme” <11529@holbertonstudents.com>
2025-09-10 14:08:13 +1000

    Fix pycodestyle

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@
 # search is the element to replace in the list = 2
 # replace is the new element = 89
 
-# if num == 2, replace that instance with 89 
+# if num == 2, replace that instance with 89
 
 def search_replace(my_list, search, replace):
     new_list = list(map(lambda x: replace if x == search else x, my_list))
