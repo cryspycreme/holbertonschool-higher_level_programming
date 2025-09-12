5f42a6cd23f69114f2f99520866e7984c4cccbe8
“cryspycreme” <11529@holbertonstudents.com>
2025-09-12 16:18:11 +1000

    Add program 3-safe_print_division.py

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+
+# Write a function that divides 2 integers and prints the result.
+# The result of the division should print on the finally: section preceded
+# by Inside result:
+# Returns the value of the division, otherwise: None
+# You have to use "{}".format() to print the result
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        result = "None"
+        return result
+    finally:
+        print("Inside Result: {}".format(result))
