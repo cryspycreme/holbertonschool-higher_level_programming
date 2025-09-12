4a042f2c9366bf2d3e3a2bd8717fdc519887b0e9
“cryspycreme” <11529@holbertonstudents.com>
2025-09-12 13:39:08 +1000

    Change try block to assume value is an int

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -5,7 +5,7 @@
 
 def safe_print_integer(value):
     try:
-        if value is int:
-            return True
-    except TypeError:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
         return False
