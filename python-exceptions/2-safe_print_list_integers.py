619d55bc5363ed702ac056c9b9124e190fe99192
“cryspycreme” <11529@holbertonstudents.com>
2025-09-12 15:59:39 +1000

    Add program 2-safe_print_list_integers.py

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# x = no of elements to access
+# x can be bigger than then length of my_list, however results in exception error
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except ValueError:
+            continue
+        except TypeError:
+            pass
+    print("")
+    return count
