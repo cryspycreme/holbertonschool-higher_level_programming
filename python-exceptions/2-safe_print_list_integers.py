69957301349d5526067b4900e5383454d7bb64c9
“cryspycreme” <11529@holbertonstudents.com>
2025-09-12 16:00:14 +1000

    SHorten sentence to fit wc

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 # x = no of elements to access
-# x can be bigger than then length of my_list, however results in exception error
+# x can be bigger than then length of my_list, however results in
+# exception error
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
