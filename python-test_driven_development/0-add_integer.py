89dbfbdf9fe26f1257a0b9251626960b10772dd3
“cryspycreme” <11529@holbertonstudents.com>
2025-09-13 16:41:54 +1000

    Fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,7 +11,6 @@ def add_integer(a, b=98):
     """
     if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
-    if b is None or (type(b) is not int and type(b) is not float)
-    :
+    if b is None or (type(b) is not int and type(b) is not float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
