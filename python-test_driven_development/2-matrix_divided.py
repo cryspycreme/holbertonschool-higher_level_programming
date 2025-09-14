ae6288a4d1947cf856bdb5a79786c8ca3ad1ab1c
“cryspycreme” <11529@holbertonstudents.com>
2025-09-14 12:26:35 +1000

    Fix matrix list of lists

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -14,18 +14,25 @@ def matrix_divided(matrix, div):
     """
     Divide each value in the matrix
     """
-    matrix_1 = []
-    if type(div) is not int and type(div) is not float:
+    
+    if not isinstance(div, (float, int)):
         raise TypeError("div must be a number")
-    if type(div) is not int and type(div) is not float:
+    if div is 0:
         raise ZeroDivisionError("division by zero")
+    
+    row_length = len(matrix[0])
+    matrix_1 = []
+    for row in matrix:
+            if len(row) != row_length:
+                raise TypeError("Each row of the matrix must have the same size")
+    
+    new_row = []
     for i in range(len(matrix)):
-        if (len(matrix[0]) is not len(matrix[1])):
-            raise TypeError("Each row of the matrix must have the same size")
         for j in range(len(matrix[i])):
             num = matrix[i][j]
-            if num is None or (type(num) is not int and type(num) is not float):
+            if not isinstance(num, (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            divided = round((matrix[i][j] / div), 2)
-            matrix_1.append(divided)
+            divided = round((num / div), 2)
+            new_row.append(divided)
+        matrix_1.append(new_row)
     return matrix_1
