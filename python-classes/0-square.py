67d359fa77f9c91263b3525fb2fae75af52eefde
“cryspycreme” <11529@holbertonstudents.com>
2025-09-15 14:39:27 +1000

    Fix pycodestyle

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 """
-This is a function that creates an empty Class called Square 
+This is a function that creates an empty Class called Square
 """
+
 
 class Square:
     """
