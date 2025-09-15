1ecc661700bc3de8e0190b495cba6adc9f50f7b6
“cryspycreme” <11529@holbertonstudents.com>
2025-09-15 14:52:28 +1000

    Add program 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This function creates a class with attribute size
+"""
+
+
+class Square:
+    """
+    define Class named Square with size attribute
+    """
+    def __init__(self, size):
+        self.__size = size
