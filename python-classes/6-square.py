ed59ab6a2abd8f56380cef8e9014a8f80e4a4efd
“cryspycreme” <11529@holbertonstudents.com>
2025-09-15 16:49:35 +1000

    Fix position error condition

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -31,9 +31,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple and (type(value[0]) is not int and type(value[1]) is not int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if (value[0] < 0 and value[1] < 0):
+        if not isinstance(value, tuple) or (value[0] < 0 and value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
