df5e01f0d8ea81bbc759bfac5d565d74a100c88e
“cryspycreme” <11529@holbertonstudents.com>
2025-09-15 16:36:18 +1000

    Add program 6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""
+This function creates a class Square
+"""
+
+
+class Square:
+    """
+    Prints and positions a square of size and position
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) and (value[0] < 0 and value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for new_line in range(self.__position[1]):
+            print("")
+        for row in range(self.__size):
+            for spaces in range(self.__position[0]):
+                print(" ", end="")
+            for i in range(self.__size):
+                print("#", end="")
+            print("")
+
+
+# x = no spaces before #
+# y = no new lines before the square
