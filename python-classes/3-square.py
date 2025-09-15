576088494efcd6320f25dd1e35608a59907c161c
“cryspycreme” <11529@holbertonstudents.com>
2025-09-15 15:27:29 +1000

    Add program 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This function creates a class Square with size attribute and error msg
+"""
+
+
+class Square:
+    """
+    Instantiates Square size and create method to calc area of square
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
