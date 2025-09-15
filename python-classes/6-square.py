379463abdc4e7a2de9a2c935daa755660105f5b8
“cryspycreme” <11529@holbertonstudents.com>
2025-09-15 16:59:58 +1000

    Fix position error condition

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -19,7 +19,7 @@ class Square:
 
     @property
     def position(self):
-        return self.position
+        return self.__position
 
     @size.setter
     def size(self, value):
@@ -39,7 +39,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.position = value
+        self.__position = value
 
     def area(self):
         return self.__size * self.__size
