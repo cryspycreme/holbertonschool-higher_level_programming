013e061441810ced2c6b353928f2139ae5e6714f
“cryspycreme” <11529@holbertonstudents.com>
2025-09-16 15:12:01 +1000

    Add program 0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This is a function that creates an empty Class called Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Define a class called Rectangle
+    """
+    ...
