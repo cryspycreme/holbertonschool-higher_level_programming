5111dfcd909ec8ab94d3ac4f429d87d91b0e6a8d
“cryspycreme” <11529@holbertonstudents.com>
2025-09-17 11:59:34 +1000

    Change if 0 condition to empty string, not tuple

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
 
     def __str__(self):
         if (self.height == 0) or (self.width == 0):
-            return ()
+            return ""
         else:
             append_row = ""
             first = True
