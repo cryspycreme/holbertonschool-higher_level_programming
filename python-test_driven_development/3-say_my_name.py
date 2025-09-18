7ca1b0c174b905bec49ddcdc5c4a3ccd15371faf
“cryspycreme” <11529@holbertonstudents.com>
2025-09-18 16:53:56 +1000

    Add docstring commetnary

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,9 +6,11 @@ Function prints My name is <first name><last name>
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    Prints first and last name in string format
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
-
