5f3cbce5be33899722f97ea493e8b0fb8cb4f1a5
“cryspycreme” <11529@holbertonstudents.com>
2025-09-18 16:05:57 +1000

    Fix pycodestyles

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,27 +10,28 @@
 This is a function that divides each row of a matrix
 """
 
+
 def matrix_divided(matrix, div):
     """
     Divide each value in the matrix
     """
-    
+
     if not isinstance(div, (float, int)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
-    
+
     matrix_1 = []
     for row in matrix:
         row_length = len(matrix[0])
         if len(row) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
-    
+
         new_row = []
         for num in row:
             if not isinstance(num, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list "
+                                "of lists) of integers/floats")
             divided = round((num / div), 2)
             new_row.append(divided)
         matrix_1.append(new_row)
