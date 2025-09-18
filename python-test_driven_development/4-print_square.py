c0a510137788f37773c88d21776545b86e5c7784
“cryspycreme” <11529@holbertonstudents.com>
2025-09-18 17:09:34 +1000

    Add docstring

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+"""
+Function to print a square
+"""
+
+
 def print_square(size):
+    """
+    Prints a square of size 'size'
+    """
     if not isinstance(size, int) or (isinstance(size, float) and size < 0):
         raise TypeError("size must be an integer")
     if size < 0:
