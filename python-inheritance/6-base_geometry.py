6ab37b8f6d83deefa98f1516312caafdd5a2dc12
“cryspycreme” <11529@holbertonstudents.com>
2025-09-22 17:41:10 +1000

    Add program 6-base_geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Geometry Module
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
