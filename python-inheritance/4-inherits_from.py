64fcf98336eb388b7cdaa2c0b5ce19ac7e7368b9
“cryspycreme” <11529@holbertonstudents.com>
2025-09-22 17:55:04 +1000

    Add program 4-inherits_from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def inherits_from(obj, a_class):
-    return True if issubclass(obj, a_class) else False
+    return True if issubclass(type(obj), a_class) and type(obj) != a_class else False
