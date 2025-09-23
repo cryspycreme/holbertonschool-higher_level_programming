c2761c073f3744e56893a340a518efcf55097516
“cryspycreme” <11529@holbertonstudents.com>
2025-09-23 10:44:25 +1000

    Add program 7-base_geometry

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Integer Validator Module
+"""
+
+
+class BaseGeometry:
+    """
+    Raise error that area is not implemented
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    """
+    Function to validate integers
+    """
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        if isinstance(name, type(value)):
+            return
