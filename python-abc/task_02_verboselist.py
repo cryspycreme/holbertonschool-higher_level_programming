7ae93df167deac9734181087886bb54b8388df75
“cryspycreme” <11529@holbertonstudents.com>
2025-09-24 16:56:14 +1000

    Add program task_02_verboselist

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, num_items):
+        super().extend(num_items)
+        print("Extended the list with [{}] items.".format(len(num_items)))
+
+    def remove(self, item):
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
+
+    def pop(self, pos=-1):
+        print("Popped [{}] from the list.".format(self[pos]))
+        super().pop(pos)
