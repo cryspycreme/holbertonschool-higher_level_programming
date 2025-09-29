9a29d1b1b087b007c7b08f4102a2c0facd0cfd8a
“cryspycreme” <11529@holbertonstudents.com>
2025-09-29 14:59:27 +1000

    Change method of print and readline

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,7 @@ def read_file(filename=""):
     Function reads file and prints to output
     """
     with open("my_file_0.txt", encoding="utf-8") as file:
-        for text in file:
-            print("{}".format(text.rstrip()))
+        lines = file.readlines()
+        
+        for line in lines:
+            print("{}".format(line.rstrip()))
