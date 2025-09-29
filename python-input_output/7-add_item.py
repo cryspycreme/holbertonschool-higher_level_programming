782d1af4be17d439144667f4bfbbaf975d888bc1
“cryspycreme” <11529@holbertonstudents.com>
2025-09-29 19:21:42 +1000

    Add program 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import sys
+import json
+
+save_json = __import__('5-save_to_json_file').save_to_json_file
+from_json = __import__('6-load_from_json_file').load_from_json_file
+
+"""
+Load, add, save module
+"""
+
+if len(sys.argv) == 1:
+    args = []
+    save_json(args, "add_item.json")
+
+args = from_json("add_item.json")
+
+if len(sys.argv) > 1:
+    for arg in sys.argv[1:]:
+        args.append(arg)
+
+save_json(args, "add_item.json")
+
+# if len(sys.argv) > 1:
+#     args = [sys.argv[1]]
+#     exist_list = from_json("add_item.json")
+#     i = 2
+#     while i < len(sys.argv):
+#         args.append(sys.argv[i])
+#         i += 1
+# else:
+#     args = []
+
+# save_json(args, "add_item.json")
