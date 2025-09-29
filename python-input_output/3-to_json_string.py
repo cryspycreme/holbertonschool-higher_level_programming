fa4585b8c85bc41451027123171e06bbafb748b1
“cryspycreme” <11529@holbertonstudents.com>
2025-09-29 16:07:36 +1000

    Add program 3-to_json_string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+
+"""
+Convert to JSON module
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Function returns JSON string of an object
+    """
+    return json.dumps(my_obj)
