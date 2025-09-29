b16891c4795b1e53e64bcdcee9d8c9370f0cd36c
“cryspycreme” <11529@holbertonstudents.com>
2025-09-29 14:56:25 +1000

    Add docstring doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+"""
+Read file module
+"""
+
 def read_file(filename=""):
+    """
+    Function reads file and prints to output
+    """
     with open("my_file_0.txt", encoding="utf-8") as file:
         for text in file:
             print("{}".format(text.rstrip()))
