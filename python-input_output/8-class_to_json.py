4c4d6dbd77e7f4681e0f3cccafba4b4f58bd10d0
“cryspycreme” <11529@holbertonstudents.com>
2025-09-30 17:34:27 +1000

    Add program 8-class_to_json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Class to JSON Module
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """
+    Function returns dictionary description for JSON object
+    """
+    dict = obj.__dict__
+    return json.dumps(dict)
