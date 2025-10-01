a55a00cff7b3b61dc7afb502fd9449f359b82921
“cryspycreme” <11529@holbertonstudents.com>
2025-10-01 14:14:23 +1000

    Add program 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""
+Student to JSON module
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiate student object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Function returns dict rep of object
+        """
+        class_dict = self.__dict__
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            result = {x: class_dict[x] for x in attrs if x in class_dict}
+        else:
+            result = class_dict
+
+        sorted_result = dict(sorted(result.items(), key=lambda item: str
+                                    (item[1])))
+        return sorted_result
+
+    def reload_from_json(self, json):
+        """
+        Function takes a dict and updates the objects attr with
+        new vaues from the dict
+        """
+        # update Class attribute to the new value in the json dict
+        # loop through the key-value pairs in the dict,
+        # if dict key == class attribute, access class attribute using
+        # class.attribute and update it to the value using json.value
+        # with open(json, encoding="utf-8") as j:
+        for key, value in json.items():
+            if key == "first_name":
+                self.first_name = value
+            if key == "last_name":
+                self.last_name = value
+            if key == "age":
+                self.age = value
