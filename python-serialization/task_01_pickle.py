140f482e839e8bf75d98a1c17a406f513174162d
“cryspycreme” <11529@holbertonstudents.com>
2025-10-01 17:26:42 +1000

    Add task_01

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+Pickling custom classes module
+"""
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        """
+        Function initialises object
+        """
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """
+        Function prints out the objects attributes in desired format
+        """
+        cobj_dict = self.__dict__
+        for key, value in cobj_dict.items():
+            new_key = key.replace("_", " ")
+            new_key = new_key.title()
+            print("{}: {}".format(new_key, value))
+
+    def serialize(self, filename):
+        """
+        Function serializes object
+        """
+        with open(filename, "wb") as srlz:
+            return pickle.dump(self, srlz)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        Function deserializes object
+        """
+        with open(filename, "rb") as dsrlz:
+            return pickle.load(dsrlz)
