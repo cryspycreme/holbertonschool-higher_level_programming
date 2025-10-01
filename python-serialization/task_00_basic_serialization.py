7b11c04e6d16c11bd55b87dab2bb020de49d5668
“cryspycreme” <11529@holbertonstudents.com>
2025-10-01 16:34:24 +1000

    Add task_00

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Basic Serialization module
+"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Function serializes a Python dict to JSON file
+    """
+    with open(filename, "w", encoding="utf-8") as srl:
+        json.dump(data, srl)
+
+
+def load_and_deserialize(filename):
+    """
+    Function deserializes a JSON File to Python dict
+    """
+    with open(filename, encoding="utf-8"):
+        json.loads(filename)
