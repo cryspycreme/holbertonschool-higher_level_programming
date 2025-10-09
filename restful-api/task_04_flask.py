41bb1f9a92bdad3d015a8e1b37e3186226e660bd
“cryspycreme” <11529@holbertonstudents.com>
2025-10-09 19:30:34 +1100

    Add task_04

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -31,20 +31,25 @@ def get_username(username):
         return (jsonify({"error": "User not found"}), 404)
 
 @app.route('/add_user', methods=['POST'])
-def add_user(users, username, data):
+def add_user():
     # {"username": "john", "name": "John", "age": 30, "city": "New York"}
     # parses JSON data into Python dict
     data = request.get_json()
+    username = data.get("username")
 
-    if not data or 'username' not in data:
+    if not username:
         return jsonify({"error": "Username is required"}), 400
     
     if username in data:
         return jsonify({"error": "Username already exists"}), 400
     
-    username = data['username']
-    users[username] = data
-    return jsonify({"message": "User added", "user": data}), 201
+    users[username] = {
+        "username": username,
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city")
+    }
+    return jsonify({"message": "User added", "user": users[username]}), 201
 
 if __name__ == "__main__":
     app.run()
