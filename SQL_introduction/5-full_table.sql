4906508420e66285705020938a33fb7ecf001e75
cryspycreme <crystalchiamm@gmail.com>
2025-10-16 05:05:31 +0000

    Add script 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,17 +1,3 @@
 --
- SELECT
-        TABLE_NAME,
-        ENGINE,
-        VERSION,
-        ROW_FORMAT,
-        TABLE_ROWS,
-        AVG_ROW_LENGTH,
-        DATA_LENGTH,
-        INDEX_LENGTH,
-        CREATE_TIME,
-        UPDATE_TIME
-    FROM
-        INFORMATION_SCHEMA.TABLES
-    WHERE
-        TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
 
