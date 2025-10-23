cc5447d076c34a707009b365cb2c273a9a9ce077
“cryspycreme” <11529@holbertonstudents.com>
2025-10-23 04:08:28 +0000

    Add script 15

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+--
+SELECT tv_shows.title AS title, tv_genres.name AS name
+FROM tv_show_genres
+LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
+
