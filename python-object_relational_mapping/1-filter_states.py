9ce0a40b9ede64f82ccf6a279e5b77bd4604e524
“cryspycreme” <11529@holbertonstudents.com>
2025-10-25 00:53:06 +0000

    Fix pycodestlyes

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 """
-Script that lists all states starting with N
+Script that returns states form the states table
 """
-
 import MySQLdb
 import sys
 
 
-def get_states_starting_with_N(username, password, dbname):
-	try:
-		with MySQLdb.connect(
-			host="localhost",
-			port=3306,
-			user=username,
-			passwd=password,
-			db=dbname
-		) as db:
-			with db.cursor() as cursor:
-				cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
-				for row in cursor.fetchall():
-					print(row)
-	except MySQLdb.Error as e:
-		print(f'Database error: {e}')
+def get_states(username, password, dbname):
+    """ Get all states from states db table starting with N"""
+    try:
+        # Connect to server (Use with for auto closing and management)
+        with MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=dbname) as db:
 
+            with db.cursor() as cur:
+                cur.execute("SELECT * FROM states WHERE name"
+                            " LIKE BINARY 'N%' ORDER BY id ASC")
+                # Fetch and display results
+                results = cur.fetchall()
+                for row in results:
+                    print(row)
 
-if __name__ == "__main__":
-	get_states_starting_with_N(sys.argv[1], sys.argv[2], sys.argv[3])
+    except MySQLdb.Error as e:
+        print(f"MySQL Error: {e}")
