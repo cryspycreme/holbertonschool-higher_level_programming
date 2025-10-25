4a397a99860bc3b13f7d3ea35708cff8a43bf9e2
“cryspycreme” <11529@holbertonstudents.com>
2025-10-25 00:41:46 +0000

    Add script 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that lists all states starting with N
+"""
+
+import MySQLdb
+import sys
+
+
+def get_states_starting_with_N(username, password, dbname):
+	try:
+		with MySQLdb.connect(
+			host="localhost",
+			port=3306 # default for localhost
+			user=username,
+			passwd=password,
+			db=dbname
+		) as db:
+			with db.cursor() as cursor:
+				cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+				for row in cursor.fetchall():
+					print(row)
+	except MySQLdb.Error as e:
+		print(f'Database error: {e}')
+
+
+if name == "__main__":
+	get_states_starting_with_N(sys.argv[1], sys.argv[2], sys.argv[3])
