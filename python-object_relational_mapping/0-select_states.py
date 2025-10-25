a8c3b5ad52987a6974cf8ac0ed82c1ea145ee547
“cryspycreme” <crystalchiamm@gmail.com>
2025-10-25 11:08:37 +1100

    Add script 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that returns states from the states table
+"""
+
+import MySQLdb
+import sys
+
+""" 0-select_states.py """
+def get_all_states(username, password, dbname):
+	""" List all states in the DB """
+	try: 
+		# Connect to server (Use with for auto closing and management)
+		with MySQLdb.connect(
+		    host="localhost",
+		    port=3306,
+		    user=username,
+		    passwd=password,
+		    db=dbname
+		) as db:
+			with db.cursor() as cursor:
+				cursor.execute("SELECT * FROM states ORDER BY id ASC")
+				for row in cursor.fetchall():
+					print(row) 
+	except MySQLdb.Error as err:
+		print(f"Database error: {err}")
+	
+if __name__ == "__main__":
+    get_all_states(sys.argv[1], sys.argv[2], sys.argv[3])
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,2 @@
+This directory contains all projects for Holberton School Module - Object Relational Mapping.
+This directory is maintained by cryspycreme.
