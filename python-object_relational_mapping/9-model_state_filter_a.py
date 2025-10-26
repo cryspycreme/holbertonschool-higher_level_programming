416395fd6d768f239abfea5ec81860f15adffa0d
“cryspycreme” <11529@holbertonstudents.com>
2025-10-26 09:27:58 +0000

    Add script 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module 8 - First state model
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from model_state import Base, State
+import sys
+
+
+def get_state_with_a(username, password, dbname):
+    """ Get states that contain the letter a """
+    # create connection between sqlalchemy to database
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}')
+
+    # bind engine to the session class and create a session instance
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # query
+    try:
+        states = session.query(State).filter(State.name.like('%a%'))
+        .order_by(State.id).all()
+        print(f'{state.id}: {state.name}')
+    finally:
+        # close session
+        session.close()
+
+
+if __name__ == "__main__":
+    get_state_with_a(sys.argv[1], sys.argv[2], sys.argv[3])
