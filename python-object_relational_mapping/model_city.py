9564133c1026f986e749f5a03d96fb9df90bace4
“cryspycreme” <11529@holbertonstudents.com>
2025-10-26 11:24:57 +0000

    Add draft model_city

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 14 - Cities in State
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker
+from model_state import Base, State
+
+
+class City(Base):
+    """ City class linked to 'cities' table in MySQL"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
+
+    state = relationship("State", backref="cities")
