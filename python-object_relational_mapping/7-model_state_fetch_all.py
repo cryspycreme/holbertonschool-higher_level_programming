087a3a342c726b8068eb66d4f19fb7fb1f7da19d
“cryspycreme” <11529@holbertonstudents.com>
2025-10-26 08:50:24 +0000

    Add script 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module 7 - All state model
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from model_state import Base, State
+import sys
+
+Base = declarative_base()
+
+if __name__ == "__main__":
+    # CLI arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    # create connection between sqlalchemy to mysql server
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}',
+            pool_pre_ping=True)
+
+    # bind engine to a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # query all states sorted by id
+    states = session.query(State).orderby(State.id).all()
+    for state in states:
+        print(f'{State.id}: {State.name}')
+
+    # close session
+    session.close()
