fdbd52fbb98dbdfe09625ff8cfb7ada2e9ee9044
“cryspycreme” <11529@holbertonstudents.com>
2025-10-26 09:36:06 +0000

    Add draft script 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module 10 - Get a state
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from model_state import Base, State
+import sys
+
+
+def get_matched_state_id(username, password, dbname, state_name):
+    """ Get state ID that match name """
+    # create connection between sqlalchemy to database
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}')
+
+    # bind engine to the session class and create a session instance
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # query 
+    try:
+        if state_name is None:
+            print("Not found")
+        else:
+            states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+            print(f'{state.id}')
+    finally:
+        # close session
+        session.close()
+
+
+if __name__ == "__main__":
+    get_matched_state_id(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
