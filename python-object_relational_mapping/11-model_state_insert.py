a6d0c776536e30671ffff701f8897312378d4f3a
“cryspycreme” <11529@holbertonstudents.com>
2025-10-26 10:58:10 +0000

    re-add script 11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -6,7 +6,7 @@ Module 11 - Add a new state
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from model_state import Base, State
-import sys
+import sys 
 
 
 def add_state(username, password, dbname):
@@ -21,10 +21,13 @@ def add_state(username, password, dbname):
 
     # query
     try:
-        state = session.query(State).filter(State.id == 2).first
-        if state is not None:
-            state.name = 'New Mexico'
-            session.commit()
+        # create a new object
+        new_state = State(name="Louisiana")
+        # add the object to the session
+        session.add(new_state)
+        # commit the session to the database
+        session.commit()
+        print(new_state.id)
     finally:
         # close session
         session.close()
