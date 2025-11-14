63a187e1a275a940742f24444e207d46940baab0
cryspycreme <crystalchiamm@gmail.com>
2025-11-14 11:39:53 +1100

    Update if condition

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -24,7 +24,7 @@ def generate_invitations(template, attendees):
     for attendee in attendees:
         template = template_copy
         for key, value in attendee.items():
-            if not value:
+            if not value or value == None:
                 template = template.replace('{' + key + '}', "N/A")
             else:
                 template= template.replace('{' + key + '}', value)
